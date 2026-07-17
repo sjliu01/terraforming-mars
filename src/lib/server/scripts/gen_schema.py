0bdbe83e741489c0f6791c18bb509a75049b356d
Anthony Liu <tonyliu.30@gmail.com>
2026-07-16 22:33:29 -0400

    Proof of concept BE <=> FE connection
    We create a proof of concept frontend route that queries from the
    backend. We want to demonstrate:
    (a) how loading data from the backend works in svelte
    (b) how the backend and frontend can share data types for the API
        (useful for not needing to duplicate game state in the future)

---
+++ src/lib/server/scripts/gen_schema.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import logging
+import os
+import yaml
+
+from fastapi.openapi.utils import get_openapi
+
+import main
+
+logger = logging.getLogger(__name__)
+
+OUTPUT_PATH = "./src/lib/server/schema/openapi.yaml"
+
+# Script to export schema to file
+if __name__ == "__main__":
+    schema = get_openapi(
+        title=main.app.title,
+        version=main.app.version,
+        routes=main.app.routes,
+    )
+
+    # Export as YAML
+    with open(OUTPUT_PATH, "w") as f:
+        yaml.dump(schema, f, default_flow_style=False)
+
+    logger.info(f"Schema exported to {OUTPUT_PATH}")
+
---
+++ src/routes/show-message/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import type { PageProps } from './$types'
+
+  let { data }: PageProps = $props()
+</script>
+
+<h1>Message</h1>
+<p>{data.message}</p>
