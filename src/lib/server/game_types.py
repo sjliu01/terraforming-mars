e707be1aeba80d934f7fd5d16f8c2802d2762076
Anthony Liu <tonyliu.30@gmail.com>
2026-07-18 13:14:10 -0400

    State: Add initial subset of game state
    We implement an initial subset of the game state and boilerplate for a
    game manager class. We also sync the types across the backend and
    frontend, and create the page `/example-game/` to proof-of-concept
    loading the game state on the frontend.
    
    The game state includes enums and initialization methods for a new game.

---
+++ src/lib/server/game_types.py
@@ -0,0 +1,203 @@
+"""TODOs
+- standard projects
+- milestones
+- awards
+
+- the tile map (5 -> 9 -> 5, and two moon tiles)
+
+- global parameter bonus steps
+
+SEE RULES AT
+https://officialgamerules.org/wp-content/uploads/2025/02/Terraforming-mars-rulebook.pdf
+"""
+
+import typing as t
+from enum import StrEnum, auto
+
+from pydantic import BaseModel
+
+# Misc types
+
+
+CorporationId = str
+CardId = str
+TileId = int
+
+
+# Enums
+# Note: should check how these enums get serialized in the API
+
+
+class GlobalParameter(StrEnum):
+    OCEAN = auto()
+    OXYGEN = auto()
+    TEMPERATURE = auto()
+
+
+class Phase(StrEnum):
+    # Do we need a GAME START?
+    TURN_ORDER = auto()
+    RESEARCH = auto()
+    ACTION = auto()
+    PRODUCTION = auto()
+    FINAL_PRODUCTION = auto()
+    FINAL_GREENERY = auto()
+
+
+class Resource(StrEnum):
+    MEGA_CREDITS = auto()
+    STEEL = auto()
+    TITANIUM = auto()
+    PLANT = auto()
+    ENERGY = auto()
+    HEAT = auto()
+    # Animal? Microbe?
+
+
+class TileType(StrEnum):
+    CITY = auto()
+    GREENERY = auto()
+    OCEAN = auto()
+    SPECIAL = auto()
+
+
+class Tag(StrEnum):
+    ANIMAL = auto()
+    BUILDING = auto()
+    CITY = auto()
+    EARTH = auto()
+    EVENT = auto()
+    JOVIAN = auto()
+    MICROBE = auto()
+    PLANT = auto()
+    POWER = auto()
+    SCIENCE = auto()
+    SPACE = auto()
+
+
+class Trigger(StrEnum):
+    AFTER_CARD_PLAY = auto()
+    AFTER_TILE_PLAY = auto()
+
+
+# Game state
+
+
+class CardState(BaseModel):
+    num_resources: int
+    action_used: bool
+    resources_can_be_taken: bool
+
+
+class PlayerState(BaseModel):
+    corporation: CorporationId | None
+    hand: list[CardId]
+    tableau: dict[CardId, CardState]
+    production: dict[Resource, int]
+    resources: dict[Resource, int]
+    terraform_rating: int
+
+    @classmethod
+    def new(cls) -> "PlayerState":
+        return PlayerState(
+            corporation=None,
+            hand=[],
+            tableau={},
+            production={resource: 0 for resource in Resource},
+            resources={resource: 0 for resource in Resource},
+            terraform_rating=20,
+        )
+
+
+class TileState(BaseModel):
+    tile: TileType | None
+    owner: int | None
+    reserved_by: int | None
+
+    @classmethod
+    def new(cls) -> "TileState":
+        return TileState(
+            tile=None,
+            owner=None,
+            reserved_by=None,
+        )
+
+
+class GenerationState(BaseModel):
+    phase: Phase
+    first_player: int
+    passed: list[bool]
+
+    @classmethod
+    def new(cls, num_players: int, first_to_play: int = 0) -> "GenerationState":
+        return GenerationState(
+            phase=Phase.RESEARCH,
+            first_player=first_to_play,
+            passed=[False] * num_players,
+        )
+
+
+class GameState(BaseModel):
+    all_cards: list[CardId]
+    deck: list[CardId]
+    player_state: list[PlayerState]
+    board_state: dict[TileId, TileState]
+    generation_state: GenerationState
+    global_parameter_targets: dict[GlobalParameter, float]
+    global_parameter_progress: dict[GlobalParameter, float]
+    cur_generation: int
+
+    @classmethod
+    def new(
+        cls,
+        num_players: int,
+        card_ids: t.Collection[CardId],
+        tile_ids: t.Collection[TileId],
+    ) -> "GameState":
+        return GameState(
+            all_cards=list(card_ids),
+            deck=[],
+            player_state=[PlayerState.new() for _ in range(num_players)],
+            board_state={_id: TileState.new() for _id in tile_ids},
+            generation_state=GenerationState.new(num_players),
+            global_parameter_targets={
+                GlobalParameter.TEMPERATURE: 8,
+                GlobalParameter.OXYGEN: 14,
+                GlobalParameter.OCEAN: 9,
+            },
+            global_parameter_progress={
+                GlobalParameter.TEMPERATURE: -30,
+                GlobalParameter.OXYGEN: 0,
+                GlobalParameter.OCEAN: 0,
+            },
+            cur_generation=1,
+        )
+
+
+# Game objects
+
+
+class Corporation(BaseModel):
+    # TODO: How to factor in, e.g., greeneries are cheaper?
+    tags: list[Tag]
+    action: t.Callable[[GameState, int], GameState]
+    passive_effect: t.Callable[[GameState, int], GameState]
+    triggered_effect: t.Callable[[Trigger], t.Callable[[GameState, int], GameState]]
+
+
+class Card(BaseModel):
+    cost: int
+    tags: list[Tag]
+    requirements: t.Callable[[GameState, int], bool]
+    action: t.Callable[[GameState, int], GameState]
+    immediate_effect: t.Callable[[GameState], GameState]
+    passive_effect: t.Callable[[GameState, int], GameState]
+    triggered_effect: t.Callable[[Trigger], t.Callable[[GameState, int], GameState]]
+    victory_points: t.Callable[[PlayerState], int]
+
+
+class Tile(BaseModel):
+    adjacent: list[TileId]  # or list tile?
+    name: str | None
+    bonuses: list[Resource]
+    isReservedForOcean: bool
---
+++ src/routes/example-game/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+</script>
+
+<h1>Example game state</h1>
+<pre>{JSON.stringify(data, null, 4)}</pre>
