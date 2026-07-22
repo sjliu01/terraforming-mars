cab68ff5a03ff357f56d945632a32cc3ff59ea0e
Anthony Liu <tonyliu.30@gmail.com>
2026-07-22 16:50:32 -0400

    State: Add initial subset of game state (#3)
    We implement an initial subset of the game state and boilerplate for a
    game manager class. We also sync the types across the backend and
    frontend, and create the page `/example-game/` to proof-of-concept
    loading the game state on the frontend.
    
    The game state includes enums and initialization methods for a new game.

--- src/lib/server/main.py
+++ src/lib/server/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from game_manager import GameManager
+from game_types import GameState
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -14,3 +16,12 @@ class MessageResponse(BaseModel):
 )
 async def get_message():
     return MessageResponse(message="peepeepoopoo")
+
+
+@app.get(
+    "/api/state/init",
+    response_model=GameState,
+)
+async def get_initial_game_state():
+    manager = GameManager.initialize_new_game(2)
+    return manager.state
